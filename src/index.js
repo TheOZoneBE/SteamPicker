d56c386453a00aa8298da34039246dfda7b2d401
Theo Dedeken <theo.dedeken@ugent.be>
2017-12-17 15:17:32 +0100

    more vue config and learning

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Test from './templates/Test.vue'
+
+new Vue({
+    el: '#app',
+    render: (h) => h(Test)
+})
---
+++ src/templates/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="test">
+        <p>{{ test }}</p>
+        <button v-on:click="update">Update</button>
+    </div>
+    
+</template>
+
+<script>
+export default  {
+    data() {
+        return {
+            test: new Date().toString()
+        }
+        
+    },
+    methods: {
+        update() {
+            this.test = 'updated'
+            console.log("update")
+        }
+    }
+}
+</script>
